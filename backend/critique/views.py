61f8e3d7c11fe2fd140081ad504d7f7328ca73af
salamentic <raniruddh99@gmail.com>
2023-05-08 18:34:05 -0400

    Added import for os in critique/views.py

--- backend/critique/views.py
+++ backend/critique/views.py
@@ -6,6 +6,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+import os
 
 import whisper
 
